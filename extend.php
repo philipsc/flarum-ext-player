6cb630ec6036e6e1b2404ab0b21d00716b37f3c1
Jason Chen <31801469+philipsc@users.noreply.github.com>
2020-08-24 14:29:09 +0800

    Support adding music
    Support adding music playing link [audio src={URL?}]

--- extend.php
+++ extend.php
@@ -13,11 +13,18 @@ use Flarum\Extend;
 use s9e\TextFormatter\Configurator;
 
 return [
+    (new Extend\Frontend('forum'))
+        ->css(__DIR__ . '/assets/forum/forum.css'),
+
     (new Extend\Formatter)
         ->configure(function (Configurator $config) {
             $config->BBCodes->addCustom(
                '[weplay src={URL?}][/weplay]',
-               '<span style="display:inline-block;width:100%"><span style="display:block;overflow:hidden;position:relative;padding-bottom:56.25%"><iframe allowfullscreen="" loading="lazy" scrolling="no" <iframe src="https://dev.wedog.cc/flarum/DPlayer/?url={URL}" style="border:0;height:100%;left:0;position:absolute;width:100%"></iframe></span></span>'
+               '<span style="display:block;overflow:hidden;position:relative;padding-bottom:56.25%"><iframe src="https://dev.wedog.cc/flarum/DPlayer/?url={URL}" allowfullscreen="" scrolling="no" style="border:0;height:100%;left:0;position:absolute;width:100%"></iframe></span>'
+            );
+            $config->BBCodes->addCustom(
+               '[audio src={URL?}][/audio]',
+               '<audio class="weplayer" controls><source src="{URL}">Your browser does not support the audio element.</audio>'
             );
         })
 ];
